d6840cb3837058ae4e28c882e587df284a0a6e08
Hinatea Ariey <Hinatea.Ariey@ext.mnhn.lu>
2026-06-11 14:41:36 +0200

    Start of the project for ISS13

---
+++ HoverflyMapAllYear.R
@@ -0,0 +1,8 @@
+######################## PROJECT: HoverflyAtlas 
+# Author: Hinatea Ariey & Julian Wittische (Musée National d'Histoire Naturelle Luxembourg)
+# Request: Hinatea Ariey
+# Start: Spring 2026
+# Data: MNHNL
+# Script objective : create map of all hoverfly data in Luxembourg through time
+
+############ Load data ----
